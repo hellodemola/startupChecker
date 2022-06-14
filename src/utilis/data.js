4226b27c3868c71437f408ddc4e8112da062566c
Ademola Onasoga <ademola.onasoga@gmail.com>
2022-06-14 23:55:40 +0100

    feat add new task

--- src/utilis/data.js
+++ src/utilis/data.js
@@ -1,34 +1,56 @@
  export const stageData = [
     {
       id: 1,
-      name: "Phase 1",
+      name: "Foundation",
       status: "In Progress",
       todo: [
         {
           id: 1,
-          name: "State Mission statement",
+          name: "Setup virtual office",
           status: true,
         }, 
         {
           id: 2,
-          name: "State Vision statement",
+          name: "State Mission & Vision statement",
+          status: false,
+        },
+         {
+          id: 3,
+          name: "Select Business Name",
+          status: false,
+        },
+          {
+          id: 3,
+          name: "Buy Domain Name",
           status: false,
         }
       ]
     },
     {
       id: 2,
-      name: "Phase 2",
+      name: "Discovery",
       status: "In Progress",
       todo: [
         {
           id: 1,
-          name: "State Mission statement",
+          name: "Create roadmap",
+          status: true,
+        }
+      ]
+   },
+    {
+      id: 3,
+      name: "Delivery",
+      status: "In Progress",
+      todo: [
+        {
+          id: 1,
+          name: "Release marketing website",
           status: true,
         },
         {
           id: 2,
-          name: "State Vision statement",
+          name: "Release MVP",
           status: false,
         }
       ]
