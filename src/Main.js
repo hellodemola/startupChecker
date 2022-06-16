a9953e06e378d495618cecd6f716e4b81c3f5fa3
Ademola Onasoga <ademola.onasoga@gmail.com>
2022-06-16 01:54:21 +0100

    add fetch url

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
+import useCallUrl from "./components/customHooks/useCallUrl";
+import useTotalCheck from "./components/customHooks/useTotalCheck";
 import PhaseComp from "./components/PhaseComp";
 
 const Main = () => {
   const { startupTasks } = useSelector((state) => state.task);
+  const { isCompleted } = useTotalCheck(startupTasks);
+  const { quote } = useCallUrl(isCompleted);
   return (
     <div className='main-wrapper'>
       <div className='container'>
         <div id='title'>
           <h1> My Startup Progress </h1>
+          <h3>{isCompleted ? 'Congratulations! All Set' : 'Keep building your hustle'}</h3>
+          <i>{isCompleted && quote && quote}</i>
         </div>
 
         <div id='each-stage'>
---
+++ src/components/customHooks/useTotalCheck.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useTotalCheck = (tasks) => {
+  const [isCompleted, setIsCompleted] = useState();
+  const taskDoneNum = tasks.filter((each) => each.status === 'Completed').length
+  const totalTasks = tasks?.length
+
+  useEffect(() => {
+    if (taskDoneNum === totalTasks) {
+     return setIsCompleted(true)
+    }
+     return setIsCompleted(false)
+  }, [taskDoneNum, totalTasks, tasks])
+  
+  return { isCompleted }
+};
+
+export default useTotalCheck;
